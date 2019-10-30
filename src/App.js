f640ed5ccb70b776e717864fac860ee693b0d366
Ben Carney <Ben.Carney@bluenile.com>
2019-10-30 15:26:35 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container, Grid, Box, Button } from '@material-ui/core';
+import Brightness1 from '@material-ui/icons/Brightness1';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Grid container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing={2}
+      >
+        <Grid item xs={2}>
+          <Box className="system-visualizer">
+            <Brightness1 className="star" />
+          </Box>
+        </Grid>
+        <Grid item xs={10}>
+          <Box>
+            <Button variant="outlined">Launch Probe</Button>
+            <p>Here is some text</p>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Share Tech Mono", monospace',
+  },
+  palette: {
+    type: 'dark',
+  },
+});
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
