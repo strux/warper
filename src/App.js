b553e01b117027e7cce9909a447ab4f2fcd607b0
Ben Carney <Ben.Carney@bluenile.com>
2019-10-31 16:17:31 -0700

    Move reducer out of render function

--- src/App.js
+++ src/App.js
@@ -9,110 +9,110 @@ import { getRandomInt } from './utils.js';
 import { maxPlanets, minPlanetSize, maxPlanetSize, minLocation, maxLocation } from './constants.js';
 import './App.css';
 
-function App() {
+const canWarp = (player) => player.fuel >= player.warpCost
 
-  const canWarp = (player) => player.fuel >= player.warpCost
+const moveActor = actor => {
+  if (!actor.target) return actor;
+  const distance = actor.target - actor.location;
+  const move = actor.speed * Math.sign(distance);
+  const delta = Math.abs(distance) - Math.abs(move)
+  const newLocation = (actor.location + (delta <= 0 ? distance : move))
+  return {
+    ...actor,
+    location: newLocation
+  }
+}
 
-  const moveActor = actor => {
-    if (!actor.target) return actor;
-    const distance = actor.target - actor.location;
-    const move = actor.speed * Math.sign(distance);
-    const delta = Math.abs(distance) - Math.abs(move)
-    const newLocation = (actor.location + (delta <= 0 ? distance : move))
+const setTarget = (actor, id, target) => {
+  if (actor.id === id) {
     return {
       ...actor,
-      location: newLocation
+      target: target
+    }
+  } else {
+    return actor;
+  }
+}
+
+const generatePlanets = (planetCount = getRandomInt(1, maxPlanets)) => {
+  const planets = [];
+
+  const generatePlanet = () => {
+    const size = getRandomInt(minPlanetSize, maxPlanetSize);
+    return {
+        size: size,
+        location: getRandomInt(minLocation, maxLocation - (size / 2))
+    }
+  };
+
+  const planetRange = planet => {
+    const radius = planet.size / 2;
+    return [planet.location - radius, planet.location + radius];
+  }
+
+  const overlap = candidate => {
+    const overlap = planets.map(planet => {
+      const [c1, c2] = planetRange(candidate);
+      const [p1, p2] = planetRange(planet);
+      return ((c1 >= p1 && c1 <= p2) || (c2 >= p1 && c2 <= p2))
+    }, false);
+    return overlap.some(o => o === true);
+  }
+
+  let id = 1;
+  while (planets.length <= planetCount) {
+    let candidate = generatePlanet();
+    if (!overlap(candidate)) {
+      candidate.id = id;
+      planets.push(candidate);
+      id++;
     }
   }
 
-  const setTarget = (actor, id, target) => {
-    if (actor.id === id) {
-      return {
-        ...actor,
-        target: target
-      }
-    } else {
-      return actor;
-    }
-  }
+  return planets;
+}
 
-  const generatePlanets = (planetCount = getRandomInt(1, maxPlanets)) => {
-    const planets = [];
+const warpDrones = (drones, player, newLocation) => {
+  return drones
+    .filter(d => d.location === player.location)
+    .map(d => ({ ...d, location: newLocation }));
+}
+const reducer = (state, action) => {
+  const { player, drones, planets } = state;
+  let newState;
 
-    const generatePlanet = () => {
-      const size = getRandomInt(minPlanetSize, maxPlanetSize);
-      return {
-          size: size,
-          location: getRandomInt(minLocation, maxLocation - (size / 2))
-      }
-    };
-
-    const planetRange = planet => {
-      const radius = planet.size / 2;
-      return [planet.location - radius, planet.location + radius];
-    }
-
-    const overlap = candidate => {
-      const overlap = planets.map(planet => {
-        const [c1, c2] = planetRange(candidate);
-        const [p1, p2] = planetRange(planet);
-        return ((c1 >= p1 && c1 <= p2) || (c2 >= p1 && c2 <= p2))
-      }, false);
-      return overlap.some(o => o === true);
-    }
-
-    let id = 1;
-    while (planets.length <= planetCount) {
-      let candidate = generatePlanet();
-      if (!overlap(candidate)) {
-        candidate.id = id;
-        planets.push(candidate);
-        id++;
-      }
-    }
-
-    return planets;
-  }
-
-  const warpDrones = (drones, player, newLocation) => {
-    return drones
-      .filter(d => d.location === player.location)
-      .map(d => ({ ...d, location: newLocation }));
-  }
-
-  const reducer = (state, action) => {
-    const { player, drones, planets } = state;
-    let newState;
-
-    switch(action.type) {
-      case 'warp':
-        if (canWarp(player)) {
-          const playerLocation = getRandomInt(50, 500);
-          newState = {
-            ...state,
-            drones: warpDrones(drones, player, playerLocation),
-            player: {...player, location: playerLocation, fuel: (player.fuel - player.warpCost)},
-            planets: generatePlanets(),
-          };
-          return newState;
-        }
-        return state;
-      case 'dispatch-drone':
+  switch(action.type) {
+    case 'warp':
+      debugger;
+      if (canWarp(player)) {
+        const playerLocation = getRandomInt(50, 500);
         newState = {
-          player,
-          drones: state.drones.map((d) => setTarget(d, action.id, action.target)),
-          planets
+          ...state,
+          drones: warpDrones(drones, player, playerLocation),
+          player: {...player, location: playerLocation, fuel: (player.fuel - player.warpCost)},
+          planets: generatePlanets(),
         };
         return newState;
-      default:
-        newState = {
-          player,
-          drones: state.drones.map(moveActor),
-          planets
-        };
-        return newState;
-    }
+      }
+      return state;
+    case 'dispatch-drone':
+      newState = {
+        player,
+        drones: state.drones.map((d) => setTarget(d, action.id, action.target)),
+        planets
+      };
+      return newState;
+    default:
+      newState = {
+        player,
+        drones: state.drones.map(moveActor),
+        planets
+      };
+      return newState;
   }
+}
+
+function App() {
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
 const width = 100;
 const height = 500;
 const maxPlanets = 5;
-const  minPlanetSize = 10;
-const  maxPlanetSize = width;
-const  minLocation = 50;
-const  maxLocation = height;
+const minPlanetSize = 10;
+const maxPlanetSize = width;
+const minLocation = 50;
+const maxLocation = height;
 
 export {
   width,
